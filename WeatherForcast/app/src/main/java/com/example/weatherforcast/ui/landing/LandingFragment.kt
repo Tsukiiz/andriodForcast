fc366c01a8c8c1b066acb4963558ad9ad406fe07
Kanin Tungwachira <kanin.twcr@gmail.com>
2023-01-09 18:33:23 +0700

    Setup data binding
    - add gradle enable databiding
    - add fragement <data>
    - add biding LandingFragment with LandingViewModel

--- WeatherForcast/app/src/main/java/com/example/weatherforcast/ui/landing/LandingFragment.kt
+++ WeatherForcast/app/src/main/java/com/example/weatherforcast/ui/landing/LandingFragment.kt
@@ -1,11 +1,15 @@
 package com.example.weatherforcast.ui.landing
 
+import android.R
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.weatherforcast.R
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.weatherforcast.databinding.FragmentLandingBinding
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,21 +25,18 @@ class LandingFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val landingViewModel: LandingViewModel by viewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_landing, container, false)
+        val binding: FragmentLandingBinding =
+            DataBindingUtil.inflate(inflater, com.example.weatherforcast.R.layout.fragment_landing, container, false) as FragmentLandingBinding
+            binding.lifecycleOwner = this
+            binding.landingViewModel = landingViewModel
+        return binding.root
     }
 
     companion object {
--- WeatherForcast/app/src/main/java/com/example/weatherforcast/ui/landing/LandingViewModel.kt
+++ WeatherForcast/app/src/main/java/com/example/weatherforcast/ui/landing/LandingViewModel.kt
@@ -1,7 +1,13 @@
 package com.example.weatherforcast.ui.landing
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class LandingViewModel: ViewModel() {
 
+class LandingViewModel: ViewModel() {
+    // Create a LiveData with a String
+    val currentName = MutableLiveData<String>()
+    fun initialize() {
+        
+    }
 }
