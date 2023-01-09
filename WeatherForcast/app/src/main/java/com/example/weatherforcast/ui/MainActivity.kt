a9eb3a0fcf18c16599e2d73b3a2bc93ad4fb0b05
Kanin Tungwachira <kanin.twcr@gmail.com>
2023-01-09 17:41:53 +0700

    Setup fragment automatically

--- WeatherForcast/app/src/main/java/com/example/weatherforcast/ui/MainActivity.kt
+++ WeatherForcast/app/src/main/java/com/example/weatherforcast/ui/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.weatherforcast.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.example.weatherforcast.R
+import com.example.weatherforcast.ui.landing.LandingFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<LandingFragment>(R.id.fragment_container_view)
+            }
+        }
     }
 }
---
+++ WeatherForcast/app/src/main/java/com/example/weatherforcast/ui/landing/LandingViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.weatherforcast.ui.landing
+
+import androidx.lifecycle.ViewModel
+
+class LandingViewModel: ViewModel() {
+
+}
